d12f661f68b3a29fa0a504c9377a8357fd57f5a8
Imputes <zhuzi@MacBook-Air.local>
2017-09-13 02:20:13 +0800

    Saving data in MongoLab

--- Instagram/ViewController.swift
+++ Instagram/ViewController.swift
@@ -11,16 +11,28 @@ import Parse
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var wusondahuImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        let object = PFObject(className: "testObject")
-        object["firstname"] = "Shao"
-        object["lastname"] = "Kahn"
-        object.saveInBackground { (done:Bool, error:Error?) in
-            if done{
-                print("Saved in server")
+    //Wrap / take image file data from UIImageView
+  let data = UIImageJPEGRepresentation(wusondahuImageView.image!, 0.5)
+  
+   //Convert taken image to file
+ let file = PFFile(name: "picture.jpb", data: data!)
+        
+        
+     //Create a class / collection / table in Heroku
+     //PFObject - to create table or some data in tanble
+        let table = PFObject(className: "messages")
+        table["sender"] = "Jordan"
+        table["receiver"] = "Goat"
+        table["picture"] = file
+        table.saveInBackground { (success:Bool, error:Error?) in
+            if success{
+   print("Saved in server")
             }else{
                 print(error)
             }
