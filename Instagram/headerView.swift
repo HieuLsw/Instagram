4ebcb8c2bdc9e518af7638dc15fdc65d248ca4b6
Imputes <zhuzi@Bobbys-MacBook-Air.local>
2017-11-23 20:55:54 +0800

    Guest Page.Follow or unfollow guest

--- Instagram/headerView.swift
+++ Instagram/headerView.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 class headerView: UICollectionReusableView {
     
@@ -39,8 +40,83 @@ class headerView: UICollectionReusableView {
         //round img layer
         setImgLayer()
     }
+  
+    
+  // clicked follow button from GuestVC
+    @IBAction func followBtn_clicked(_ sender: Any) {
     
     
+    let title = button.title(for: .normal)
+    
+    //to follow
+if title == "FOLLOW" {
+    let object = PFObject(className: "follow")
+    object["follower"] = PFUser.current()?.username
+    object["following"] = (guestName.last)!
+    object.saveInBackground(block: { (success, error) in
+        
+        if success {
+            
+            self.button.setTitle("FOLLOWING", for: .normal)
+            self.button.backgroundColor = .green
+            
+            // send follow notification
+            let newsObj = PFObject(className: "news")
+            newsObj["by"] = PFUser.current()?.username
+            newsObj["ava"] = PFUser.current()?.object(forKey: "ava") as! PFFile
+            newsObj["to"] = (guestName.last)!
+            newsObj["owner"] = ""
+            newsObj["uuid"] = ""
+            newsObj["type"] = "follow"
+            newsObj["checked"] = "no"
+            newsObj.saveEventually()
+            
+            
+        } else {
+            print(error!.localizedDescription)
+        }
+
+    })
+}else{
+    
+ // unfollow
+    let query = PFQuery(className: "follow")
+    query.whereKey("follower", equalTo: (PFUser.current()!.username)!)
+    query.whereKey("following", equalTo: (guestName.last)!)
+    
+    query.findObjectsInBackground(block: { (objects, error)  in
+        if error == nil {
+            
+            for object in objects! {
+                object.deleteInBackground(block: { (success, error)  in
+    if success {
+    self.button.setTitle("FOLLOW", for: .normal)
+        self.button.backgroundColor = .lightGray
+                        
+// delete follow notification
+let newsQuery = PFQuery(className: "news")
+newsQuery.whereKey("by", equalTo: (PFUser.current()!.username)!)
+newsQuery.whereKey("to", equalTo: (guestName.last)!)
+                        newsQuery.whereKey("type", equalTo: "follow")
+    
+newsQuery.findObjectsInBackground(block: { (objects, error)  in
+
+if error == nil {
+    
+for object in objects! {object.deleteEventually()}
+     }
+  })
+} else {print(error!.localizedDescription)}
+    })
+}
+} else {
+            print(error!.localizedDescription)
+        }
+    })
+    
+        }
+}
+        
 }// headerView class over line
 
 extension headerView{
