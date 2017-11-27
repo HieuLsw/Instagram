8b5b56817daf45d8492a7dce47814f241d212cc2
Imputes <zhuzi@Bobbys-MacBook-Air.local>
2017-11-27 14:59:42 +0800

    Upload Page.Creating user interface and alignment

---
+++ Instagram/uploadVC.swift
@@ -0,0 +1,41 @@
+//
+//  uploadVC.swift
+//  Instagram
+//
+//  Created by Bobby Negoat on 11/27/17.
+//  Copyright © 2017 Mac. All rights reserved.
+//
+
+import UIKit
+
+class uploadVC: UIViewController {
+
+    @IBOutlet weak var picImg: UIImageView!
+    
+    @IBOutlet weak var titleTxt: UITextView!
+
+    @IBOutlet weak var publishBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
