c02a76f1082ccc3d7f38144e098641fc9a1f67a7
Imputes <zhuzi@Bobbys-MacBook-Air.local>
2017-12-01 23:42:48 +0800

    Comment Page.Aligenment

--- Instagram/commentCell.swift
+++ Instagram/commentCell.swift
@@ -21,12 +21,51 @@ class commentCell: UITableViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        
+        //configue cell views layout
+        configueCellLayout()
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
+        //super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
     }
 
+}//commentVC class over lineanimated
+
+//custom functions
+extension commentCell{
+    
+    fileprivate func configueCellLayout(){
+    
+_ = [avaImg,usernameBtn,commentLbl,dateLbl].map{$0.translatesAutoresizingMaskIntoConstraints = false
+        }
+        
+     //ava image layout
+    NSLayoutConstraint.init(item: avaImg, attribute: .height, relatedBy: .equal, toItem: avaImg, attribute: .notAnAttribute, multiplier: 1.0, constant: 60).isActive = true
+    NSLayoutConstraint.init(item: avaImg, attribute: .height, relatedBy: .equal, toItem: avaImg, attribute: .width, multiplier: 1/1, constant: 0.0).isActive = true
+     NSLayoutConstraint.init(item: avaImg, attribute: .left, relatedBy: .equal, toItem: commentCell().contentView, attribute: .leftMargin, multiplier: 1.0, constant: 0.0).isActive = true
+     NSLayoutConstraint.init(item: avaImg, attribute: .centerX, relatedBy: .equal, toItem: commentCell().contentView, attribute: .centerX, multiplier: 1.0, constant: 0.0).isActive = true
+      
+    //date label layout
+  dateLbl.heightAnchor.constraint(equalToConstant: 21).isActive = true
+  dateLbl.widthAnchor.constraint(equalToConstant: 30).isActive = true
+  NSLayoutConstraint(item: dateLbl, attribute: .top, relatedBy: .equal, toItem: avaImg, attribute: .top, multiplier: 1.0, constant: 0.0).isActive = true
+  
+   // username button layout
+  NSLayoutConstraint(item: usernameBtn, attribute: .height, relatedBy: .equal, toItem: commentLbl, attribute: .height, multiplier: 1.0, constant: 0.0).isActive = true
+   NSLayoutConstraint(item: usernameBtn, attribute: .left, relatedBy: .equal, toItem: avaImg, attribute: .right, multiplier: 1.0, constant: 25.0).isActive = true
+    NSLayoutConstraint(item: usernameBtn, attribute: .top, relatedBy: .equal, toItem: avaImg, attribute: .top, multiplier: 1.0, constant: 0.0).isActive = true
+    NSLayoutConstraint.init(item: usernameBtn, attribute: .right, relatedBy: .lessThanOrEqual, toItem: dateLbl, attribute: .left, multiplier: 1.0, constant: 53.0).isActive = true
+        
+   //comment label layout
+    commentLbl.heightAnchor.constraint(equalToConstant: 21.0).isActive = true
+    NSLayoutConstraint(item: commentLbl, attribute: .right, relatedBy: .equal, toItem: commentCell().contentView, attribute: .rightMargin, multiplier: 1.0, constant: 0.0).isActive = true
+    NSLayoutConstraint.init(item: commentLbl, attribute: .bottom, relatedBy: .equal, toItem: avaImg, attribute: .bottom, multiplier: 1.0, constant: 0.0).isActive = true
+    NSLayoutConstraint(item: commentLbl, attribute: .left, relatedBy: .equal, toItem: avaImg, attribute: .right, multiplier: 1.0, constant: 25.0).isActive = true
 }
+}
+
+
+
--- Instagram/commentVC.swift
+++ Instagram/commentVC.swift
@@ -15,11 +15,14 @@ class commentVC: UIViewController {
     @IBOutlet weak var commentTxt: UITextView!
     
     @IBOutlet weak var sendBtn: UIButton!
-        
+ 
+    @IBOutlet weak var contentView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
+      //set views layout
+     configueVCAlignment()
     }
 
     override func didReceiveMemoryWarning() {
@@ -32,5 +35,43 @@ class commentVC: UIViewController {
     
     
     }
+    
+}//commentVC class over line
+
+//custom functions
+extension commentVC{
+    
+    //set views layout
+    fileprivate func configueVCAlignment(){
+  
+     //avoid the issue to auto layout
+_ = [tableView,sendBtn,commentTxt,contentView].map{$0.translatesAutoresizingMaskIntoConstraints = false}
+ 
+//table view layout
+  tableView.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
+   tableView.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
+   tableView.rightAnchor.constraint(equalTo: self.view.rightAnchor).isActive = true
+    
+  //content view layout
+   contentView.heightAnchor.constraint(equalTo: self.view.heightAnchor, multiplier: 105 / 812).isActive = true
+    contentView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
+     contentView.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
+      contentView.rightAnchor.constraint(equalTo: self.view.rightAnchor).isActive = true
+     contentView.topAnchor.constraint(equalTo: self.tableView.bottomAnchor).isActive = true
+  
+   //comment text view layout
+ commentTxt.leftAnchor.constraint(equalTo: self.view.layoutMarginsGuide.leftAnchor).isActive = true
+ commentTxt.topAnchor.constraint(equalTo: self.contentView.topAnchor, constant: 5).isActive = true
+  commentTxt.centerXAnchor.constraint(equalTo: self.sendBtn.centerXAnchor).isActive = true
+  commentTxt.heightAnchor.constraint(equalToConstant: 43).isActive = true
+
+   //send button layout
+  sendBtn.rightAnchor.constraint(equalTo: self.contentView.layoutMarginsGuide.rightAnchor).isActive = true
+sendBtn.heightAnchor.constraint(equalTo: commentTxt.heightAnchor).isActive = true
+sendBtn.widthAnchor.constraint(equalToConstant: 46).isActive = true
+sendBtn.leftAnchor.constraint(equalTo: commentTxt.rightAnchor, constant: 10).isActive = true
+        
+    }
+    
     
 }
