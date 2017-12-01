8f3ea744e03aba850c5dc817be6b9ee87317eefa
Imputes <zhuzi@Bobbys-MacBook-Air.local>
2017-12-01 09:04:04 +0800

    Post Page.Creating user interface

---
+++ Instagram/postCell.swift
@@ -0,0 +1,42 @@
+//
+//  postCell.swift
+//  Instagram
+//
+//  Created by Bobby Negoat on 12/1/17.
+//  Copyright © 2017 Mac. All rights reserved.
+//
+
+import UIKit
+
+class postCell: UITableViewCell {
+
+    @IBOutlet weak var avaImg: UIImageView!
+    
+    @IBOutlet weak var usernameBtn: UIButton!
+    
+    @IBOutlet weak var dateLbl: UILabel!
+
+    @IBOutlet weak var picImg: UIImageView!
+
+    @IBOutlet weak var likeBtn: UIButton!
+ 
+    @IBOutlet weak var commentBtn: UIButton!
+    
+    @IBOutlet weak var moreBtn: UIButton!
+    
+    @IBOutlet weak var titleLbl: UILabel!
+    
+    @IBOutlet weak var likeLbl: UILabel!
+        
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Instagram/postVC.swift
@@ -0,0 +1,26 @@
+//
+//  postVC.swift
+//  Instagram
+//
+//  Created by Bobby Negoat on 12/1/17.
+//  Copyright © 2017 Mac. All rights reserved.
+//
+
+import UIKit
+
+class postVC: UIViewController {
+
+    @IBOutlet weak var customTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
